5a09cf5540589ec20855311bf76a5471217d7ff4
tsh321 <tsh321@nyu.edu>
2020-06-26 22:36:44 -0400

    changing database settings/adding desktop model

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Desktops(models.Model):
+	name = models.CharField(max_length=100)
+	user = models.CharField(max_length=50)
+	locationType = models.CharField(max_length=50)
+	location = models.CharField(max_length=50)
+	dateLastModified = models.DateTimeField(editable=False)
+	serialNumber = models.CharField(max_length=50)
+	macAddress = models.CharField(max_length=50)
+	IPAddress = models.CharField(max_length=50)
+	OS = models.CharField(max_length=50)
+	userType = models.CharField(max_length=50)
+	Notes = models.CharField(max_length=50)
+	lastUpdatedUser = models.CharField(max_length=50)
+
+	# Create your models here.

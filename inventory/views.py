a5a249dc157b959a304462013e848602993302ff
tsh321 <tsh321@nyu.edu>
2020-07-01 14:18:41 -0400

    now saves desktops to databases

--- inventory/views.py
+++ inventory/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 
-from .models import DesktopForm
+from .models import *
 # Create your views here.
 
 def index(request):
@@ -13,18 +13,20 @@ def createDesktop(request):
 		# create a form instance and populate it with data from the request:
 		form = DesktopForm(request.POST)
 		# check whether it's valid:
+
 		if form.is_valid():
 			data = request.POST.copy()
-			#newDesktop = new Desktop(name=data.get('name'), locationType=data.get('locationType'), location=data.get('name'),
-			#	dateLastModified=datetime.datetime.now(), Notes=data.get('Notes'), modelName=data.get('modelName'), user=data.get('user'),
-			#	serialNumber=data.get('serialNumber'), macAddress=data.get('macAddress'), IPAddress=data.get('IPAddress'),
-			#	OS=data.get('OS'), userType=data.get('userType'))
+			newDesktop = Desktops(name=data.get('name'), locationType=data.get('locationType'), location=data.get('name'),
+				dateLastModified=datetime.datetime.now(), Notes=data.get('Notes'), modelName=data.get('modelName'), user=data.get('user'),
+				serialNumber=data.get('serialNumber'), macAddress=data.get('macAddress'), IPAddress=data.get('IPAddress'),
+				OS=data.get('OS'), userType=data.get('userType'))
 
-			# process the data in form.cleaned_data as required
-			# ...
-			# redirect to a new URL:
-			response = 'The name of the desktop is %s'
-			return HttpResponse("Response: " + response % name)
+			newDesktop.save()
+
+			time = datetime.datetime.now()
+
+			response = 'The current time is %s'
+			return HttpResponse("Response: " + response % time)
 
 	# if a GET (or any other method) we'll create a blank form
 	else:

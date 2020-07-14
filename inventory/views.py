ebb93ed3dc3f884945a1194941897d137c3793a0
Thomas Han <thomashan@Thomass-MacBook-Pro.local>
2020-07-13 22:05:46 -0400

    added 404 errors for views

--- inventory/views.py
+++ inventory/views.py
@@ -12,27 +12,31 @@ def index(request):
 	return HttpResponse("Hello, world.")
 
 def createObject(request, mySlug):
-	if slugIsValid(mySlug):
+	if slugIsValid(mySlug) != True:
+		return HttpResponseNotFound(mySlug.capitalize() + " is not a valid object.")
 
-		constructor = globals()[mySlug.capitalize()]
-		subObject = constructor()
-		myList = list()
-		if request.method == 'POST':
-			f = commonObjectForm(request.POST)
-			if f.is_valid():
-				subObject = formDataToObject(request.POST.items(), mySlug, True) #inserts the request data into object, and takes care of token, date, and slug as well
-				subObject.save()
-				return HttpResponseRedirect(reverse('inventory:displayAllObjects', args = (mySlug,)))
+	constructor = globals()[mySlug.capitalize()]
+	subObject = constructor()
+	myList = list()
+	if request.method == 'POST':
+		f = commonObjectForm(request.POST)
+		if f.is_valid():
+			subObject = formDataToObject(request.POST.items(), mySlug, True) #inserts the request data into object, and takes care of token, date, and slug as well
+			subObject.save()
+			return HttpResponseRedirect(reverse('inventory:displayAllObjects', args = (mySlug,)))
 
-		# if a GET (or any other method) we'll create a blank form
-		else:
-			f = commonObjectForm()
-			f = createDynamicForm(subObject)
+	# if a GET (or any other method) we'll create a blank form
+	else:
+		f = commonObjectForm()
+		f = createDynamicForm(subObject)
 
-			return render(request, 'inventory/createObject.html', {'form': f, 'mySlug': mySlug.capitalize})
+		return render(request, 'inventory/createObject.html', {'form': f, 'mySlug': mySlug.capitalize})
 
 
 def editDesktop(request, secret_id, mySlug):
+
+	if slugIsValid(mySlug) != True:
+		return HttpResponseNotFound(mySlug.capitalize() + " is not a valid object.")
 
 	editObject = commonObject.objects.get(token = secret_id)
 	editObject = getattr(editObject, mySlug)
@@ -66,6 +70,10 @@ def editDesktop(request, secret_id, mySlug):
 
 
 def displayAllObjects(request, mySlug, sortBy = ''):
+	
+	if slugIsValid(mySlug) != True:
+		return HttpResponseNotFound(mySlug.capitalize() + " is not a valid object.")
+
 	objectName = re.sub("([a-z])([A-Z])","\g<1> \g<2>", mySlug) #uncamelcases the slug for visual purposes
 	objectname = objectName.lower()
 	if slugIsValid(mySlug):

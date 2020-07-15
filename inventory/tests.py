dc47c9370656f5ceabcd97139180bc74854826f1
Thomas Han <thomashan@Thomass-MBP.home>
2020-07-15 09:38:58 -0400

    added two more tests

--- inventory/tests.py
+++ inventory/tests.py
@@ -10,12 +10,21 @@ class ObjectTestCase(TestCase):
 		commonObjects = commonObject.objects.filter(name = "test")
 		self.assertEqual(commonObjects.count(), 2)
 
+	def existsAsDesktop(self):
+		desktop =  Desktops.objects.get(name = "test")
+		self.assertEqual(desktop.user, 'user1')
+
+	def existsAsNotebook(self):
+		notebook = Notebooks.objects.get(name = "test")
+		self.assertEqual(notebook.user, 'user2')
+
 	def databaseLinksExist(self):
 		r = commonObject.objects.get(user = "user1")
 		p = commonObject.objects.get(user = "user2")
 		desktop = getattr(r, r.slug)
 		self.assertEqual(r.__class__.__name__, "Desktops")
 		self.assertEqual(p.__class__.__name__, "Notebooks")
+
 
 
 

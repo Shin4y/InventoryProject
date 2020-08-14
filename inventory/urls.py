bf20bf5b97ea99192577a1e9de33e5ddd8403998
tsh321 <tsh321@nyu.edu>
2020-08-14 10:41:14 -0400

    moving to laptop

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,10 +7,10 @@ app_name = 'inventory'
 
 urlpatterns = [
 	path('', views.index, name = 'index'),
-	path('/webapps/inventory/equipment/<str:secret_id>/edit', views.editObject, name = 'editObject'),
-	path('<slug:mySlug>/create/', views.createObject, name = 'createObject'),
-	path('<slug:mySlug>/', views.displayAllObjects, name = 'displayAllObjects'),
-	path('<slug:mySlug>/sort=<str:sortBy>/', views.displayAllObjects, name = 'displayAllObjects'),
-	path('<slug:mySlug>/batch/', views.batchReplace, name = 'batchReplace'),
-	path('<slug:mySlug>/toStorage/<str:room>+<str:name>/<str:building>/', views.toStorage, name = 'toStorage'),
+	path(r^'webapps/inventory/equipment/<str:secret_id>/edit', views.editObject, name = 'editObject'),
+	path(r^'<slug:mySlug>/create/', views.createObject, name = 'createObject'),
+	path(r^'<slug:mySlug>/', views.displayAllObjects, name = 'displayAllObjects'),
+	path(r^'<slug:mySlug>/sort=<str:sortBy>/', views.displayAllObjects, name = 'displayAllObjects'),
+	path(r^'<slug:mySlug>/batch/', views.batchReplace, name = 'batchReplace'),
+	path(r^'<slug:mySlug>/toStorage/<str:room>+<str:name>/<str:building>/', views.toStorage, name = 'toStorage'),
 	]
